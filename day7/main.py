f466c5ec478f215e19bc3f9d56bee96729d1ee8a
Dawid Kulikowski <git@dawidkulikowski.pl>
2022-12-07 11:47:15 +0000

    Day 7

---
+++ day7/main.py
@@ -0,0 +1,19 @@
+data = open("day7/input.txt", 'r').readlines()
+current_path = []
+structure = {"/": 0}
+for l in data:
+    match l.strip().split(" "):
+        case ["$", "cd", "/"]:
+            current_path.clear()
+        case ["$", "cd", ".."]:
+            current_path.pop()
+        case ["$", "cd", path]:
+            current_path.append(path)
+            structure["/" if not current_path else "/" + '/'.join(current_path)] = structure["/" if not current_path else "/" + '/'.join(current_path)] if ("/" if not current_path else "/" + '/'.join(current_path)) in structure.keys() else 0
+        case ["$", "ls"] | ["dir", *_]:
+            pass
+        case [fsize, fname]:
+            structure["/" if not current_path else "/" + '/'.join(current_path)] += int(fsize)
+            for k in (x for x, _ in structure.items() if ("/" if not current_path else "/" + '/'.join(current_path)).startswith(x) and x != ("/" if not current_path else "/" + '/'.join(current_path))):
+                structure[k] += int(fsize)
+print(f"Part 1: {sum([x for x in structure.values() if x <= 100000])} Part 2: {min(filter(lambda x: 70000000 - structure['/'] + x >= 30000000 , list(structure.values())))}")
