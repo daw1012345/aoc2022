bf6c13c1e7feb375b6671fe18e3de89c307a2b71
Dawid <git@dawidkulikowski.pl>
2022-12-10 22:04:44 +0100

    Day 9

---
+++ day9/main.py
@@ -0,0 +1,42 @@
+STRAIGHT_MOVES = [(0,1),(1,0), (0,-1), (-1,0)]
+DIAG_MOVES = [(1,1), (1,-1), (-1,1), (-1,-1)]
+SNAKE_LEN = 10
+
+snake, current_tail = (0,0), (0,0)
+visited = {(0,0)}
+snake = [(0,0) for _ in range(SNAKE_LEN)]
+
+def move_snake():
+    for i in range(len(snake)):
+        if i == 0: 
+            continue
+        if snake[i] in [(snake[i-1][0]-xd, snake[i-1][1]-yd) for (xd,yd) in DIAG_MOVES+STRAIGHT_MOVES+[(0,0)]]: 
+            continue
+        common = set([(snake[i][0]+xd, snake[i][1]+yd) for (xd,yd) in STRAIGHT_MOVES]).intersection(set([(snake[i-1][0]+xd, snake[i-1][1]+yd) for (xd,yd) in STRAIGHT_MOVES]))
+        if common:
+            snake[i] = common.pop()
+            continue
+        common = set([(snake[i][0]+xd, snake[i][1]+yd) for (xd,yd) in DIAG_MOVES]).intersection(set([(snake[i-1][0]+xd, snake[i-1][1]+yd) for (xd,yd) in DIAG_MOVES+STRAIGHT_MOVES]))
+        snake[i] = common.pop()
+    visited.add(snake[-1])
+
+for cmd in open("day9/input.txt", 'r').readlines():
+    dir = cmd.split(' ')[0]
+    n = int(cmd.split(' ')[1])
+    for _ in range(n):
+        match dir:
+            case 'R':
+                snake[0] = (snake[0][0] + 1, snake[0][1])
+            case 'L':
+                snake[0] = (snake[0][0] - 1, snake[0][1])
+            case 'U':
+                snake[0] = (snake[0][0], snake[0][1]+1)
+            case 'D':
+                snake[0] = (snake[0][0], snake[0][1]-1)
+        move_snake()
+
+print(len(visited))
+
+
+
+
